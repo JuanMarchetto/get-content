be1017b076598ebe65d39d70bc69cd176e623d2b
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-09-18 09:38:20 -0300

    UI

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
-import { useState, useEffect } from "react"
+import { useRouter } from 'next/router'
+import { useRef, useCallback } from 'react'
 
 export default function App() {
+  const router = useRouter()
+  const inputElement = useRef(null);
+
+  const callbackRef = useCallback(inputElement => {
+    if (inputElement) {
+      inputElement.focus();
+    }
+  }, []);
 
 
-  useEffect(
-    () => {
-      fetch(`https://www.clarin.com/`)
-        .then(response => console.log(response))
-        /*.then(json => {
-          console.log(json.results)
-        })*/
-        .catch(err => console.log(err));
-    },
-    []
-  )
- return <h1>hola</h1>;
+  const handleChange = (e) => {
+    e.target.value
+    router.push(`/api/${e.target.value}`)
   }
+
+  return (
+    <div
+      className="flex justify-center h-[100vh] items-center"
+    >
+      <input
+        onChange={handleChange}
+        className="border border-stone-900 p-2 w-[80vw] h-[50px]"
+        ref={callbackRef}
+      />
+    </div>
+  )
+}
